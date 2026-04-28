e8a2fff7f3c28e8a2cf4610b9e32f5df0feb88ac
jorgechato <hack@jorgechato.com>
2026-04-29 07:40:46 +0900

    change: add bongo cat

--- rules.mk
+++ rules.mk
@@ -9,15 +9,18 @@ AUDIO_ENABLE = no           # Audio output
 RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 OLED_ENABLE= yes     # OLED display
+WPM_ENABLE = yes
 
-# Advanced optimizations
-GRAVE_ESC_ENABLE = no       # Save ~100 bytes
-MAGIC_ENABLE = yes           # Save ~500 bytes
-SPACE_CADET_ENABLE = yes     # Save ~350 bytes
-LTO_ENABLE = yes            # Link Time Optimization - saves significant space
+# # Advanced optimizations
+# GRAVE_ESC_ENABLE = no       # Save ~100 bytes
+# MAGIC_ENABLE = yes           # Save ~500 bytes
+# SPACE_CADET_ENABLE = yes     # Save ~350 bytes
+# LTO_ENABLE = yes            # Link Time Optimization - saves significant space
 
 # If you want to change the display of OLED, you need to change here
-ifeq ($(OLED_ENABLE), yes)
-    SRC +=  ./lib/layer_state_reader.c \
-            ./lib/keylogger.c
-endif
+# ifeq ($(OLED_ENABLE), yes)
+# 	SRC +=  ./lib/rgb_state_reader.c \
+#         ./lib/layer_state_reader.c \
+#         ./lib/logo_reader.c \
+#         ./lib/keylogger.c
+# endif
